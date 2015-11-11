3a5479c363924f5a3c3309a31f09f70cce2eedeb
szymonlyszkowski <szymonlyszkowski@gmail.com>
2015-11-11 02:52:45 +0100

    Madaline network finished. Works for 3 letter samples: X,Y,Z. Requires refactoring plus addtional training samples with extension for all letters recognition.

--- MadalineNetwork.py
+++ MadalineNetwork.py
@@ -1,12 +1,14 @@
 import random
 import numpy
+from Neuron import Neuron
+from training_set_letters import letters
 
 
 class MadalineNetwork:
-    def __init__(self, training_set, neuron_amount_in_network, neural_network_neuron):
+    def __init__(self, training_set, neuron_amount_in_network, two_dimensional_array_weights):
         self.__training_set = training_set
-        self.__madaline_network_neurons = self.__init_madaline_network_with_neurons(neuron_amount_in_network, neural_network_neuron)
-        self.__init_madaline_network_neurons_with_weights(self.__madaline_network_neurons, neural_network_neuron)
+        self.__madaline_network_neurons = self.__init_madaline_network_with_neurons(neuron_amount_in_network)
+        self.__madaline_network_neurons = self.__init_madaline_network_neurons_with_weights(self.__madaline_network_neurons, two_dimensional_array_weights)
 
     @staticmethod
     def __initialize_madaline_network_neurons_with_random_weights(training_set):
@@ -23,15 +25,56 @@ class MadalineNetwork:
                                                                                                      vectors_of_weights_amount))
         for neuron_index, neuron in enumerate(network_neurons):
             neuron.initialize_neuron_weights(two_dimensional_weights_array[neuron_index])
+        return network_neurons
 
     @staticmethod
-    def __init_madaline_network_with_neurons(neuron_amount_in_network, neural_network_neuron):
+    def __init_madaline_network_with_neurons(neuron_amount_in_network):
         network_neurons = []
         for _ in xrange(neuron_amount_in_network):
-            network_neurons.append(neural_network_neuron)
+            network_neurons.append(Neuron())
         print 'Network neurons created are: %s' % network_neurons
         return network_neurons
 
+    def __return_normalized_vector(self, input_vector):
+        flatten_vector = input_vector.flatten()
+        vector_length = numpy.linalg.norm(flatten_vector)
+        normalized_vector = []
+        for vector_element in flatten_vector:
+            normalized_vector.append(vector_element/vector_length)
+        return normalized_vector
+
+    def normalize_neural_network_neurons_weights(self, networks_neurons):
+        neuron_array_weights_normalized = []
+        for neuron in networks_neurons:
+            neuron_with_modified_weights = Neuron()
+            neuron_with_modified_weights.weights = self.__return_normalized_vector(neuron.weights)
+            neuron_array_weights_normalized.append(neuron_with_modified_weights)
+        return neuron_array_weights_normalized
+
+    def run_madaline(self):
+        self.__training_set = self.__return_normalized_vector(self.__training_set)
+        self.__madaline_network_neurons = self.normalize_neural_network_neurons_weights(self.__madaline_network_neurons)
+        output_results = []
+        for neuron in self.__madaline_network_neurons:
+            neuron_output = self.__computue_output_value_from_neuron_in_madaline_network(self.__training_set, neuron.weights)
+            output_results.append(neuron_output)
+        highest_output = max(output_results)
+        highest_output_index = output_results.index(highest_output)
+        print 'Neuron with highest output was: %s and its index is %s' % (highest_output, highest_output_index)
+        return highest_output, highest_output_index
+
+
+    def __computue_output_value_from_neuron_in_madaline_network(self, training_set, neuron_weights):
+        result = 0
+        for corresponding_index, training_sample in enumerate(training_set):
+            result += training_sample*neuron_weights[corresponding_index]
+        return result
+
 
 if __name__ == '__main__':
-    print numpy.cos([1, 2, 3])
+    training_set = numpy.array([[1, 1, 1, 1],
+                                [0, 0, 1, 0],
+                                [0, 1, 0, 0],
+                                [1, 1, 1, 1]])
+    letter = letters.Letters()
+    MadalineNetwork(training_set,3, [letter.get_X_four_by_four(), letter.get_Y_four_by_four(), letter.get_Z_four_by_four()]).run_madaline()
