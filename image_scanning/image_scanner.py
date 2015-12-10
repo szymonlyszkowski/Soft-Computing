6c1d7de9566ee988ab76d6b136c3bb0de690486e
szymonlyszkowski <szymonlyszkowski@gmail.com>
2015-12-10 20:12:54 +0100

    added unit test for image from random frame retrieval.

--- image_scanning/image_scanner.py
+++ image_scanning/image_scanner.py
@@ -3,52 +3,49 @@ import random
 import numpy
 
 
-def get_maxium_allowed_index(image_array, frame_side_length):
-    """
-    Returns maxium value for index to be used for getting random frame for size == frame_side_length
-    from image_array
-    :type frame_side_length: integer
-    :type image_array: 1d or 2d array
-    """
-    maximum_allowed_index = len(image_array) - frame_side_length
-    return maximum_allowed_index
-
-
 class ImageScanner(object):
-
-    def __init__(self, image_path):
-        self.image_path = image_path
-
-    def get_image_as_array(self):
-        image = Image.open(self.image_path)
-        return numpy.array(image)
-
-
-
-    def __scan_image(self, image_array, frame_side_length):
+    def __init__(self):
         pass
 
-    def __get_random_frame(self, image_array, maximum_allowed_index, frame_side_length):
-        end_index_row, start_index_row = self.__get_random_allowed_indices(frame_side_length, maximum_allowed_index)
-        end_index_column, start_index_column = self.__get_random_allowed_indices(frame_side_length, maximum_allowed_index)
+    @staticmethod
+    def get_image_as_array(image_path):
+        image = Image.open(image_path)
+        return numpy.array(image)
+
+    def get_random_frame(self, image_as_array, frame_side_length):
+        maximum_allowed_index = self.__get_maxium_allowed_index(image_as_array, frame_side_length)
+        end_index, start_index = self.__get_random_allowed_indices(frame_side_length, maximum_allowed_index)
         one_dimensional_array = []
-        for row in xrange(start_index_row,end_index_row):
-            for column in xrange(start_index_column, end_index_column):
-                one_dimensional_array.append(image_array[row][column])
+        for row in xrange(start_index, end_index):
+            for column in xrange(start_index, end_index):
+                one_dimensional_array.append(image_as_array[row][column])
         return one_dimensional_array
 
-    def __get_random_allowed_indices(self, frame_side_length, maximum_allowed_index):
-        start_index = random.uniform(0, maximum_allowed_index)
+    @staticmethod
+    def __get_random_allowed_indices(frame_side_length, maximum_allowed_index):
+        start_index = random.randint(0, maximum_allowed_index)
         end_index = start_index + frame_side_length
         return end_index, start_index
 
-    def __compute_frame_value(self, frame_array):
+    @staticmethod
+    def __compute_frame_value(frame_array):
         return numpy.mean(frame_array)
 
+    @staticmethod
+    def __get_maxium_allowed_index(image_array, frame_side_length):
+        """
+        Returns maxium value for index to be used for getting random frame for size == frame_side_length
+        from image_array
+        :type frame_side_length: integer
+        :type image_array: 1d or 2d array
+        """
+        maximum_allowed_index = len(image_array) - frame_side_length
+        return maximum_allowed_index
 
 
 if __name__ == '__main__':
-    scanner = ImageScanner('images/lena-512-grayscale.bmp')
-    image_array = scanner.get_image_as_array()
-    print len(image_array)
+    scanner = ImageScanner()
+    image_array = scanner.get_image_as_array('images/lena-512-grayscale.bmp')
+    image_frame = scanner.get_random_frame(image_array, 100)
+    print len(image_frame)
     numpy.savetxt('images/image_array.txt', image_array, delimiter=',', fmt="%s")
---
+++ image_scanning/image_scanner_tests.py
@@ -0,0 +1,36 @@
+import unittest
+import numpy
+from image_scanning.image_scanner import ImageScanner
+
+
+class ImageScannerTests(unittest.TestCase):
+    def test_get_random_frame_length_from_image_8_by_8(self):
+        # GIVEN
+        mock_image_array = numpy.zeros((512, 512))
+        scanner = ImageScanner()
+        # WHEN
+        random_frame = scanner.get_random_frame(mock_image_array, 8)
+        # THEN
+        self.assertEqual(len(random_frame), 64)
+
+    def test_get_random_frame_length_from_image_4_by_4(self):
+        # GIVEN
+        mock_image_array = numpy.zeros((512, 512))
+        scanner = ImageScanner()
+        # WHEN
+        random_frame = scanner.get_random_frame(mock_image_array, 4)
+        # THEN
+        self.assertEqual(len(random_frame), 16)
+
+    def test_get_random_frame_length_from_image_16_by_16(self):
+        # GIVEN
+        mock_image_array = numpy.zeros((512, 512))
+        scanner = ImageScanner()
+        # WHEN
+        random_frame = scanner.get_random_frame(mock_image_array, 16)
+        # THEN
+        self.assertEqual(len(random_frame), 256)
+
+
+if __name__ == '__main__':
+    unittest.main()
